1b5b0ddf7c7f398379afea63a1d878e9e91e9c32
EmilyUry <ury.emily@gmail.com>
2022-02-28 12:03:45 -0500

    updated figure

--- Figure 1.R
+++ Figure 1.R
@@ -33,23 +33,27 @@ pal
 
 
 world <- borders("world", colour="#8a8cb5", fill="#414487bb") # create a layer of borders
-map <- ggplot() + world + ylim(-55,85) + theme_void() +
-  geom_point(aes(x=x$Long, y = x$Lat), color = "black", size = 1) +
+map <- ggplot() + world + ylim(-55,100) + theme_void() +
+  geom_point(aes(x=x$Long, y = x$Lat), color = "black", pch = 21, fill = "white", size = 0.8) +
   labs(title="(a) Location of studied wetlands ")+
   theme(plot.margin = margin(t = 0, r = 1, b = 0.5, l = 1, unit = "cm")) +
-  theme(plot.title = element_text(hjust = 1, size = 10, face = "bold"))
+  theme(plot.title = element_text(hjust = 1, size = 7, face = "bold"))
 
 map
 
 ## (b) - histogram of wetland size distribition
 
-b <- ggplot(x, aes(Area_m2)) +
+n <- x %>%
+  group_by(WetlandID) %>%
+  summarize(area = mean(Area_m2))
+
+b <- ggplot(n, aes(area)) +
   geom_histogram(bins = 10, color = "#414487bb", fill = "#414487bb" ) +
   scale_x_log10() +
   theme_classic() +
   labs(title = "(b) Wetland size distribution", x = expression(paste("Area (", m^2, ")")))+
-  theme(plot.margin = margin(t = 0.2, r = 1, b = 0.3, l = 1.5, unit = "cm")) +
-  theme(plot.title = element_text(hjust = 1.2, size = 10, face = "bold"))
+  theme(plot.margin = margin(t = 0.2, r = 0.3, b = 0.3, l = 0.2, unit = "cm")) +
+  theme(plot.title = element_text(hjust = 0.1, size = 7, face = "bold"))
 
 b 
 
@@ -73,20 +77,20 @@ SRP.source.percent <- round(SRP.source/(SRP.sink+SRP.source)*100,1)
 
 behavior <- c("source", "sink", "source", "sink")
 species <- c("TP", "TP", "SRP", "SRP")
-num <- c(32, 144, 49, 126)
-label_ypos <- c(125, 10, 125, 10)
+num <- c(144, 32, 126, 49)
+label_ypos <- c(130, 10, 130, 10)
 label_text <- c("source \n(18%)", "sink", "source \n(28%)", "sink")
 data <- data.frame(behavior, species, num, label_ypos, label_text)
 
-c <- ggplot(data, aes(x = factor(species, level = c("TP", "SRP")), y = (percent), fill = behavior)) +
+c <- ggplot(data, aes(x = factor(species, level = c("TP", "SRP")), y = (num), fill = behavior)) +
   geom_bar(stat = "identity") +
-  geom_text(aes(y = label_ypos, label = label_text), vjust = 0, hjust = "middle", color = "white", size = 3.5)+
+  geom_text(aes(y = label_ypos, label = label_text), vjust = 0, hjust = "middle", color = "white", size = 2.5)+
   theme_classic() +
   labs(title = "(c) Wetland sink/source behavior", x = " ", y = "n" ) +
   scale_fill_manual(values = c("#414487bb", "#414487ff")) + 
   theme(legend.position = "none") +
-  theme(plot.margin = margin(t = 0, r = 2, b = 0, l = 1, unit = "cm")) +
-  theme(plot.title = element_text(hjust = 0.6, size = 10, face = "bold"))
+  theme(plot.margin = margin(t = 0, r = 0.5, b = 0, l = 1, unit = "cm")) +
+  theme(plot.title = element_text(hjust = 0.6, size = 7, face = "bold"))
   
 c
 
@@ -103,18 +107,21 @@ names(dd) <- c("Source", "Flow", "count")
 d <- ggplot(dd, aes(Source, factor(Flow, level = c("n.s.", "continuous, constant", "continuous, variable", 
                                               "intermittent, constant", "intermittent, variable")), fill = count)) +
   geom_tile() +
-  geom_text(aes(label=count), size = 3.5, color = "white") +
+  geom_text(aes(label=count), size = 2.5, color = "white") +
   scale_fill_gradient(low="#41448777", high ="#414487ff") +
-  theme_classic()+
+  theme_classic() +
+  scale_y_discrete(labels = c( "n.s.", "cont., const.", "cont., var",  "int.,  const.", "int., var.")) +
+  scale_x_discrete(labels = c("Ag", "Urban", "WTP")) +
   labs(title = "(d) Wetland inflow charactersistics", y = " ", x = " ") +
-  theme(plot.margin = margin(t = 0, r = 0.5, b = 0, l = 0.5, unit = "cm"), legend.position = "none") +
-  theme(plot.title = element_text(hjust = 1, size = 10, face = "bold"))
+  theme(plot.margin = margin(t = 0, r = 0.2, b = 0, l = 0.2, unit = "cm"), legend.position = "none") +
+  theme(plot.title = element_text(hjust = 1, size = 7, face = "bold"))
 
 
 
 
 ### (e) - number of studies in each year
 n <- x %>%
+  filter(Catchment_Type == "ag") %>%
   group_by(Source) %>%
   summarize(num = n())
 
@@ -123,20 +130,71 @@ n <- n %>%
 n[is.na(n)] <- '1994'
 n$Year <- as.numeric(n$Year)
 
-n <- n %>%
+ag <- n %>%
   group_by(Year) %>%
+  summarize(num = n()) %>%
+  mutate(type = "Ag")
+
+n <- x %>%
+  filter(Catchment_Type == "urban") %>%
+  group_by(Source) %>%
   summarize(num = n())
 
-p <- ggplot(data =n, aes(x=Year, y = num)) + 
-  geom_bar(stat = "identity", fill = "#414487bb") +
+n <- n %>%
+  separate(Source, into = c("Author", "Year"), sep = " ")
+n[is.na(n)] <- '1994'
+n$Year <- as.numeric(n$Year)
+
+urb <- n %>%
+  group_by(Year) %>%
+  summarize(num = n()) %>%
+  mutate(type = "Urban")
+
+n <- x %>%
+  filter(Catchment_Type == "WWTP") %>%
+  group_by(Source) %>%
+  summarize(num = n())
+
+n <- n %>%
+  separate(Source, into = c("Author", "Year"), sep = " ")
+n[is.na(n)] <- '1994'
+n$Year <- as.numeric(n$Year)
+
+WTP <- n %>%
+  group_by(Year) %>%
+  summarize(num = n()) %>%
+  mutate(type = "WTP")
+
+e <- rbind(ag, urb, WTP) %>%
+  mutate(type = fct_relevel(type, "WTP", "Urban", "Ag"))
+
+
+
+p <- ggplot(data = e, aes(fill = type, x=Year, y = num)) + 
+  geom_bar(position = "stack", stat = "identity") +
   theme_classic() +
+  scale_fill_manual(values = c("#8a8cb588", "#41448799", "#414487")) +
   labs(title = "(e) Study publication year", y = "n", x = " ") +
   theme(plot.margin = margin(t = 0, r = 1, b = 0, l = 1, unit = "cm")) +
-  theme(plot.title = element_text(hjust = -0.10 , size = 10, face = "bold"))
+  theme(plot.title = element_text(hjust = -0.10 , size = 7, face = "bold")) +
+  theme(legend.title = element_blank(), legend.text = element_text(size = 7),
+        legend.key.size = unit(0.3, 'cm'))
 p
 
 
+
+
+
+
+
+
+
+
+tiff(filename = "figures/Figure1.tiff", height=3600, width=3600, units= "px", res=800, compression= "lzw")
+
 grid.arrange(map, b, c, d, p, nrow = 3, layout_matrix = rbind(c(1,2), c(3,4), c(5,5)))
+
+dev.off()
 
 
 
