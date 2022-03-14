d2fa891ba836b897f172d3f6baef7f63ea326c95
EmilyUry <ury.emily@gmail.com>
2022-03-14 16:45:35 -0400

    figure 1 with updated data

--- Figure 1.R
+++ Figure 1.R
@@ -22,8 +22,12 @@ library(gridExtra)
 
 setwd("C:/Users/Emily Ury/OneDrive - University of Waterloo/Wetlands_local/Data_files/Wetland_P_Analysis/")
 x <- read.csv("Wetland_P_Toy_Data2.csv", header = T)
+x <- read.csv("Wetland_P_Clean.csv", header = T)
 head(x)
 
+
+unique(x$Source)
+unique(x$WetlandID)
 
 options(scipen = 100) ## gets out of scientific notation
 pal <- viridis(6)
@@ -49,7 +53,10 @@ n <- x %>%
 
 b <- ggplot(n, aes(area)) +
   geom_histogram(bins = 10, color = "#414487bb", fill = "#414487bb" ) +
-  scale_x_log10() +
+  scale_x_log10(breaks=c(1, 100, 1000000)) + 
+  annotation_logticks(base = 10, sides = "b", outside = TRUE, short = unit(0, "cm"), 
+                      mid = unit(0.0, "cm"), long = unit(0.1, "cm"))  +
+  coord_cartesian(clip = "off") +
   theme_classic() +
   labs(title = "(b) Wetland size distribution", x = expression(paste("Area (", m^2, ")")))+
   theme(plot.margin = margin(t = 0.2, r = 0.3, b = 0.3, l = 0.2, unit = "cm")) +
@@ -63,28 +70,28 @@ b
 
 x$ret <- ifelse(x$TP_Retention_percent > 0, "pos", "neg")
 table(x$ret)
-32/(32+144)
+57/(57+223)
 TP.source <- table(x$ret)[1]
 TP.sink <- table(x$ret)[2]
 TP.source.percent <- round(TP.source/(TP.sink+TP.source)*100,1)
 
 x$PO4ret <- ifelse(x$SRP_Retention_percent > 0, "pos", "neg")
 table(x$PO4ret)
-49/(49+126)
+88/(88+191)
 SRP.source <- table(x$PO4ret)[1]
 SRP.sink <- table(x$PO4ret)[2]
 SRP.source.percent <- round(SRP.source/(SRP.sink+SRP.source)*100,1)
 
 behavior <- c("source", "sink", "source", "sink")
 species <- c("TP", "TP", "SRP", "SRP")
-num <- c(144, 32, 126, 49)
-label_ypos <- c(130, 10, 130, 10)
-label_text <- c("source \n(18%)", "sink", "source \n(28%)", "sink")
+num <- c(TP.sink, TP.source, SRP.sink, SRP.source)
+label_ypos <- c(200, 10, 200, 10)
+label_text <- c("source \n(20%)", "sink", "source \n(32%)", "sink")
 data <- data.frame(behavior, species, num, label_ypos, label_text)
 
 c <- ggplot(data, aes(x = factor(species, level = c("TP", "SRP")), y = (num), fill = behavior)) +
   geom_bar(stat = "identity") +
-  geom_text(aes(y = label_ypos, label = label_text), vjust = 0, hjust = "middle", color = "white", size = 2.5)+
+  geom_text(aes(y = label_ypos, label = label_text), vjust = 0, hjust = "middle", color = "white", size = 2.5, face = "bold")+
   theme_classic() +
   labs(title = "(c) Wetland sink/source behavior", x = " ", y = "n" ) +
   scale_fill_manual(values = c("#414487bb", "#414487ff")) + 
@@ -100,8 +107,9 @@ c
 
 table(x$Catchment_Type)
 table(x$Water_regime)
+table(x$Wetland_Type)
 
-dd <- data.frame(table(x$Catchment_Type, x$Water_regime))
+dd <- data.frame(table(x$Wetland_Type, x$Water_regime))
 names(dd) <- c("Source", "Flow", "count")
 
 d <- ggplot(dd, aes(Source, factor(Flow, level = c("n.s.", "continuous, constant", "continuous, variable", 
@@ -110,14 +118,15 @@ d <- ggplot(dd, aes(Source, factor(Flow, level = c("n.s.", "continuous, constant
   geom_text(aes(label=count), size = 2.5, color = "white") +
   scale_fill_gradient(low="#41448777", high ="#414487ff") +
   theme_classic() +
-  scale_y_discrete(labels = c( "n.s.", "cont., const.", "cont., var",  "int.,  const.", "int., var.")) +
-  scale_x_discrete(labels = c("Ag", "Urban", "WTP")) +
-  labs(title = "(d) Wetland inflow charactersistics", y = " ", x = " ") +
+  scale_y_discrete(labels = c( "n.s.", "CC", "CV",  "IC", "IV")) +
+  scale_x_discrete(labels = c("Constr.", "Mesoc.", "Natural", "Rest.")) +
+  labs(title = "(d) Flow regime by wetland type", y = " ", x = " ") +
   theme(plot.margin = margin(t = 0, r = 0.2, b = 0, l = 0.2, unit = "cm"), legend.position = "none") +
-  theme(plot.title = element_text(hjust = 1, size = 7, face = "bold"))
+  theme(plot.title = element_text(hjust = 1, size = 7, face = "bold")) +
+  theme(axis.text.x = element_text(hjust = 0.1, size = 8, angle = 335))
 
 
-
+d
 
 ### (e) - number of studies in each year
 n <- x %>%
@@ -166,7 +175,7 @@ WTP <- n %>%
   mutate(type = "WTP")
 
 e <- rbind(ag, urb, WTP) %>%
-  mutate(type = fct_relevel(type, "WTP", "Urban", "Ag"))
+  mutate(type = fct_relevel(type, "Urban", "WTP", "Ag"))
 
 
 
@@ -175,20 +184,84 @@ p <- ggplot(data = e, aes(fill = type, x=Year, y = num)) +
   theme_classic() +
   scale_fill_manual(values = c("#8a8cb588", "#41448799", "#414487")) +
   labs(title = "(e) Study publication year", y = "n", x = " ") +
-  theme(plot.margin = margin(t = 0, r = 1, b = 0, l = 1, unit = "cm")) +
+  theme(plot.margin = margin(t = 0, r = 0.1, b = 0, l = 1, unit = "cm")) +
   theme(plot.title = element_text(hjust = -0.10 , size = 7, face = "bold")) +
   theme(legend.title = element_blank(), legend.text = element_text(size = 7),
         legend.key.size = unit(0.3, 'cm'))
 p
 
 
-
-
-
-
-
-
-
+## panel E alternative
+{
+# 
+# n <- x %>%
+#   filter(Wetland_Type == "Constructed") %>%
+#   group_by(Source) %>%
+#   summarize(num = n())
+# n <- n %>%
+#   separate(Source, into = c("Author", "Year"), sep = " ")
+# n[is.na(n)] <- '1994'
+# n$Year <- as.numeric(n$Year)
+# Con <- n %>%
+#   group_by(Year) %>%
+#   summarize(num = n()) %>%
+#   mutate(type = "Constructed")
+# 
+# n <- x %>%
+#   filter(Wetland_Type == "Restored") %>%
+#   group_by(Source) %>%
+#   summarize(num = n())
+# n <- n %>%
+#   separate(Source, into = c("Author", "Year"), sep = " ")
+# n[is.na(n)] <- '1994'
+# n$Year <- as.numeric(n$Year)
+# Res <- n %>%
+#   group_by(Year) %>%
+#   summarize(num = n()) %>%
+#   mutate(type = "Restored")
+# 
+# n <- x %>%
+#   filter(Wetland_Type == "Mesocosm") %>%
+#   group_by(Source) %>%
+#   summarize(num = n())
+# n <- n %>%
+#   separate(Source, into = c("Author", "Year"), sep = " ")
+# n[is.na(n)] <- '1994'
+# n$Year <- as.numeric(n$Year)
+# Mes <- n %>%
+#   group_by(Year) %>%
+#   summarize(num = n()) %>%
+#   mutate(type = "Mesocosm")
+# 
+# n <- x %>%
+#   filter(Wetland_Type == "Natural") %>%
+#   group_by(Source) %>%
+#   summarize(num = n())
+# n <- n %>%
+#   separate(Source, into = c("Author", "Year"), sep = " ")
+# n[is.na(n)] <- '1994'
+# n$Year <- as.numeric(n$Year)
+# Nat <- n %>%
+#   group_by(Year) %>%
+#   summarize(num = n()) %>%
+#   mutate(type = "Natural")
+# 
+# 
+# e <- rbind(Con, Res, Mes, Nat) %>%
+#   mutate(type = fct_relevel(type, "Natural", "Mesocosm", "Restored", "Constructed"))
+# 
+# 
+# p <- ggplot(data = e, aes(fill = type, x=Year, y = num)) + 
+#   geom_bar(position = "stack", stat = "identity") +
+#   theme_classic() +
+#   scale_fill_manual(values = c("#8a8cb588", "#41448799", "#414487", "#0a0c40")) +
+#   labs(title = "(e) Study publication year", y = "n", x = " ") +
+#   theme(plot.margin = margin(t = 0, r = 1, b = 0, l = 1, unit = "cm")) +
+#   theme(plot.title = element_text(hjust = -0.10 , size = 7, face = "bold")) +
+#   theme(legend.title = element_blank(), legend.text = element_text(size = 7),
+#         legend.key.size = unit(0.3, 'cm'))
+# p
+}
 
 tiff(filename = "figures/Figure1.tiff", height=3600, width=3600, units= "px", res=800, compression= "lzw")
 
