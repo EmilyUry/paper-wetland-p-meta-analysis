f13d5f06ab35016dace6486da2b572f974b6d917
EmilyUry <ury.emily@gmail.com>
2022-03-31 09:42:06 -0400

    new figs

--- scratch_figures.R
+++ scratch_figures.R
@@ -115,7 +115,7 @@ legend("topleft", levels(x$Water_regime),
 
 
 
-
+####### Flow regime
 
 plot(x$SRP_Retention_percent, x$TP_Retention_percent, 
      pch = 16,
@@ -131,6 +131,27 @@ abline(h=0, col = 'gray50', lwd =1, lty = 2)
 abline(v=0, col = 'gray30', lwd = 1, lty = 2)
 legend("bottomleft", levels(x$Water_regime), pch = 16,
        pt.cex = 2, col = c("#2b821fbb", "#bd4ad4bb","#345bebbb", "#e34327bb", "#a1a1a1bb" ))
+
+
+### wetland type
+table(x$Wetland_Type)
+
+plot(x$SRP_Retention_percent, x$TP_Retention_percent, 
+     pch = 16,
+     cex = 1.5,
+     #col = "#515151bb",
+     col = c(  "#e34327bb","#2b821fbb",  "#345bebbb","#a1a1a1bb" )[x$Wetland_Type],
+     xlim = c(-250, 105), 
+     ylim = c(-150, 105), 
+     xlab = "SRP % Retention",
+     ylab = "TP % Retention")
+abline(1,1)
+abline(h=0, col = 'gray50', lwd =1, lty = 2)
+abline(v=0, col = 'gray30', lwd = 1, lty = 2)
+legend("bottomleft", levels(x$Wetland_Type), pch = 16,
+       pt.cex = 2, col = c("#e34327bb", "#2b821fbb",   "#345bebbb","#a1a1a1bb" ))
+
+
 
 
 ## mean SRP retention
@@ -204,6 +225,90 @@ abline(h=0, col = 'gray50', lwd =1, lty = 2)
 abline(v=0, col = 'gray30', lwd = 1, lty = 2)
 legend("bottomleft", c(" low", "med", " high"), pch = 16,
        pt.cex = 2, col = c("#FF000099",  "#7F007F99","#0000FF99"))
+
+
+
+
+### flow volume in
+hist(log10(x$Inflow_m3_yr))
+x$flow.norm <- x$Inflow_m3_yr/x$Area_m2
+
+
+rbPal <- colorRampPalette(c('red','blue'))
+x$col <- rbPal(3)[as.numeric(cut(x$Inflow_m3_yr, breaks = c(0, 1000, 10000,  Inf)))]
+x$alpha <- rep(99, 279)
+x$col2 <- paste(x$col, x$alpha, sep = "")
+x$col2[which(x$col2 == "NA99" )] <- "#99999999"
+
+plot(x$SRP_Retention_percent, x$TP_Retention_percent, 
+     pch = 16,
+     cex = 1.5,
+     col = x$col2,
+     xlim = c(-250, 105), 
+     ylim = c(-150, 105), 
+     xlab = "SRP % Retention",
+     ylab = "TP % Retention")
+abline(1,1)
+abline(h=0, col = 'gray50', lwd =1, lty = 2)
+abline(v=0, col = 'gray30', lwd = 1, lty = 2)
+legend("bottomleft", c(" low", "med", " high"), pch = 16,
+       pt.cex = 2, col = c("#FF000099",  "#7F007F99","#0000FF99"))
+
+
+
+
+
+### flow anomally
+library(tidyverse)
+Flow <- aggregate(Inflow_m3_yr ~ Source + WetlandID, data = x, FUN = mean) %>%
+        mutate(Unique_ID = paste(Source, WetlandID))
+names(Flow)[3] <- "mean_flow"
+
+
+wetlands <- x %>%
+        mutate(Unique_ID = paste(Source, WetlandID)) %>%
+        group_by(Unique_ID) %>%
+        summarize(num = n()) %>%
+        filter(num > 1) %>%
+        left_join(Flow, by = "Unique_ID") %>%
+        select(c('Source', 'WetlandID', 'mean_flow'))
+
+
+data <- x %>%
+        left_join(wetlands, by = c("Source", "WetlandID")) %>%
+        mutate(flow_anom = (Inflow_m3_yr - mean_flow)/mean_flow*100)
+data <- data[-c(249:259),] 
+data <- data[-c(98:107),]                              ###### a bunch of rows report the average flow for all yrs. need to remove these
+data <- data[-c(54:82),]
+
+
+## Red/Blue binary
+rbPal <- colorRampPalette(c('red','blue'))
+data$col <- paste(rbPal(10)[as.numeric(cut(data$flow_anom, breaks = 10))], "85", sep = "")
+data <- data %>%
+        mutate(col = replace(col, col == "NA85", "#8a8a8a")) %>%
+        mutate(col = replace(col, flow_anom == 0, "#8a8a8a")) %>%
+        mutate(col = replace(col, flow_anom < 0, "#FF000085")) %>%
+        mutate(col = replace(col, flow_anom > 0, "#0000FF85")) 
+data$flow_anom[is.na(data$flow_anom)] <- 0
+plot(data$SRP_Retention_percent, data$TP_Retention_percent, 
+     pch = 16,
+     cex = abs(data$flow_anom)/30+1,
+     col = data$col,
+     xlim = c(-250, 105), 
+     ylim = c(-150, 105), 
+     xlab = "SRP % Retention",
+     ylab = "TP % Retention")
+abline(1,1)
+abline(h=0, col = 'gray50', lwd =1, lty = 2)
+abline(v=0, col = 'gray30', lwd = 1, lty = 2)
+legend("bottomright", c("High flow year", "Low flow year"), pch = 16,
+       pt.cex = 2, col = c("#0000FF85","#FF000085"))
+
+
+
+
+
 
 
 
