6a1ab1cc33a79b86dedff4251d02723e0fb475ca
Emily Ury <ury.emily@gmail.com>
2022-09-08 10:49:14 -0400

    added new summary stats

--- Summary_stats.R
+++ Summary_stats.R
@@ -14,7 +14,9 @@
 #'           (iii) retention efficiency (%)
 
 setwd("C:/Users/Emily Ury/OneDrive - University of Waterloo/Wetlands_local/Data_files/Wetland_P_Analysis/")
+setwd("C:/Users/uryem/OneDrive - University of Waterloo/Wetlands_local/Data_files/Wetland_P_Analysis")  #laptop
 
+library(tidyverse)
 
 ## Data set-up
 x <- read.csv("Wetland_P_Clean3.csv", header = T)
@@ -102,5 +104,96 @@ summary <- rbind(TP_IN, TP_Retention, TP_Retention_percent, SRP_IN, SRP_Retentio
 
 
 
+plot(density(x$TP_load_in_g_m2_yr))
+
+
+summary
+
+
+
+
+
+x$ret <- ifelse(x$TP_Retention_percent > 0, "pos", "neg")
+TP.source <- table(x$ret)[1]
+TP.sink <- table(x$ret)[2]
+TP.source.percent <- round(TP.source/(TP.sink+TP.source)*100,1)
+
+x$PO4ret <- ifelse(x$SRP_Retention_percent > 0, "pos", "neg")
+SRP.source <- table(x$PO4ret)[1]
+SRP.sink <- table(x$PO4ret)[2]
+SRP.source.percent <- round(SRP.source/(SRP.sink+SRP.source)*100,1)
+
+behavior <- c("source", "sink", "source", "sink")
+species <- c("TP", "TP", "SRP", "SRP")
+num <- c(TP.sink, TP.source, SRP.sink, SRP.source)
+label_ypos <- c(245, 10, 245, 10)
+label_text <- c("16%", " ", "25%", " ")
+data <- data.frame(behavior, species, num, label_ypos, label_text)
+
+A <- ggplot(data, aes(x = factor(species, level = c("TP", "SRP")), y = (num), fill = behavior)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(y = label_ypos, label = label_text), vjust = 0, hjust = "middle",
+            color = "white", size = 4.5, fontface = "bold")+
+  theme_classic(base_size = 16) +
+  theme(legend.position = "right", legend.direction = "vertical", legend.text = element_text(size = 13), 
+        legend.key.size = unit(0.5,"cm")) +
+  labs(x = " ", y = "n (site-years)", fill = " " ) +
+  scale_fill_manual(values = c("#414547", "#9ba4a8"), labels = c("source", "sink"))  +
+  scale_x_discrete(labels = c("TP", "PO4"))
+A
+
+x <- x[which(x$Wetland_Type != "Mesocosm"),]
+
+
+TP.source <- table(x$ret)[1]
+TP.sink <- table(x$ret)[2]
+TP.source.percent <- round(TP.source/(TP.sink+TP.source)*100,1)
+
+SRP.source <- table(x$PO4ret)[1]
+SRP.sink <- table(x$PO4ret)[2]
+SRP.source.percent <- round(SRP.source/(SRP.sink+SRP.source)*100,1)
+
+behavior <- c("source", "sink", "source", "sink")
+species <- c("TP", "TP", "SRP", "SRP")
+num <- c(TP.sink, TP.source, SRP.sink, SRP.source)
+label_ypos <- c(180, 10, 180, 10)
+label_text <- c("20.2%", " ", "32.7%", " ")
+data <- data.frame(behavior, species, num, label_ypos, label_text)
+
+A <- ggplot(data, aes(x = factor(species, level = c("TP", "SRP")), y = (num), fill = behavior)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(y = label_ypos, label = label_text), vjust = 0, hjust = "middle",
+            color = "white", size = 4.5, fontface = "bold")+
+  theme_classic(base_size = 16) +
+  theme(legend.position = "right", legend.direction = "vertical", legend.text = element_text(size = 13), 
+        legend.key.size = unit(0.5,"cm")) +
+  labs(x = " ", y = "n (site-years)", fill = " " ) +
+  scale_fill_manual(values = c("#414547", "#9ba4a8"), labels = c("source", "sink"))  +
+  scale_x_discrete(labels = c("TP", "PO4"))
+A
+
+
+
+
+## TP median release/retention
+Source <- x[which(x$TP_Retention < 0),]
+summary(Source$TP_Retention)
+Sink <- x[which(x$TP_Retention > 0),]
+summary(Sink$TP_Retention)
+
+## SRP median release/retention
+Source <- x[which(x$SRP_Retention < 0),]
+summary(Source$SRP_Retention)
+Sink <- x[which(x$SRP_Retention > 0),]
+summary(Sink$SRP_Retention)
+
+
+## percent of construncted wetlands that are source
+
+constructed <- x[which(x$Wetland_Type == "Constructed"),]
+nrow(constructed[which(constructed$TP_Retention < 0),])
+21/136 # 15% TP sink
+nrow(constructed[which(constructed$SRP_Retention < 0),])
+47/136 ## 35% SRP source
 
 
