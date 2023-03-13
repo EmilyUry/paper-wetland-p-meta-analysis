870f9a9df86ab407570f7f4879333fc9c3a8cd28
Emily Ury <ury.emily@gmail.com>
2023-03-13 14:11:45 -0400

    added plot labels (A B C D)

--- box plots wetland types.R
+++ box plots wetland types.R
@@ -8,6 +8,7 @@ setwd("C:/Users/uryem/OneDrive - University of Waterloo/Wetlands_local/Data_file
 library(ggplot2)
 library(tidyverse)
 library(cowplot)
+library(rstatix)
 
 x <- read.csv("Wetland_P_Clean3.csv", header = T)
 
@@ -88,57 +89,57 @@ sig$SRP <- c("A", "A", "AC", "ABC", "BC", "C")
 
 pal <- c('#e5a77f', '#3e783f', '#6fabd9')
 
-a <- ggplot(y, aes(x = group, y = TP_Retention_percent, fill = group )) +
-  geom_boxplot(position = position_dodge2(preserve = "single")) +
-  scale_fill_manual(values = pal) +
-  scale_x_discrete(labels = c("M", "R/C", "W")) +
-  coord_cartesian(ylim = c(-180,120)) +
-  facet_grid(.~flow, space = "free") +
-  theme_bw(base_size = 10) +
-  geom_text(data = summary, aes(label = count),
-            position = position_dodge(width = 1.0), size = 2.9, color = "gray50")+
-  geom_text(data = sig, aes(label = TP), 
-            position = position_dodge(width = 1.0), size = 2.5) +
-  ylab("Retention (%)") +
-  xlab(" ") +
-  theme(plot.margin = unit(c(t = 0, r = 0.5, b = 0, l = 0.5), "cm"),
-        legend.position = "top",
-        legend.title = element_blank(),
-        plot.title = element_text( hjust = -0.2, vjust = -10, size = 14)) +
-  ggtitle("TP")
-
-
-
-b <- ggplot(y, aes(x = group, y = SRP_Retention_percent, fill = group )) +
-  geom_boxplot(position = position_dodge2(preserve = "single")) +
-  scale_fill_manual(values = pal) +
-  scale_x_discrete(labels = c("M", "R/C", "W")) +
-  coord_cartesian(ylim = c(-180,120)) +
-  facet_grid(.~flow, space = "free") +
-  theme_bw(base_size = 10) +
-  geom_text(data = summary, aes(label = count),
-            position = position_dodge(width = 1.0), size = 2.9, color = "gray50")+
-  geom_text(data = sig, aes(label = SRP), 
-            position = position_dodge(width = 1.0), size = 2.5) +
-  ylab("Retention (%)") +
-  xlab(" ") +
-  theme(plot.margin = unit(c(t = 0, r = 0.5, b = 0, l = 0.5), "cm"),
-        legend.position = "none",
-        legend.title = element_blank(),
-        plot.title = element_text( hjust = -0.2, size = 14)) +
-  ggtitle(expression("PO"[4]^"3-"))
-
-
-plot_grid(a,b, nrow = 2, labels = c(" ", " "), rel_heights = c(1,0.9))
-
-
-##### paper version figure 4
-
-tiff(filename = "figures/Type_box_facet.tif", height=5, width=4, units= "in", res=800, compression= "lzw")
-
-plot_grid(a,b, nrow = 2, labels = c(" ", " "), rel_heights = c(1,0.85))
-
-dev.off()
+# a <- ggplot(y, aes(x = group, y = TP_Retention_percent, fill = group )) +
+#   geom_boxplot(position = position_dodge2(preserve = "single")) +
+#   scale_fill_manual(values = pal) +
+#   scale_x_discrete(labels = c("M", "R/C", "W")) +
+#   coord_cartesian(ylim = c(-180,120)) +
+#   facet_grid(.~flow, space = "free") +
+#   theme_bw(base_size = 10) +
+#   geom_text(data = summary, aes(label = count),
+#             position = position_dodge(width = 1.0), size = 2.9, color = "gray50")+
+#   geom_text(data = sig, aes(label = TP), 
+#             position = position_dodge(width = 1.0), size = 2.5) +
+#   ylab("Retention (%)") +
+#   xlab(" ") +
+#   theme(plot.margin = unit(c(t = 0, r = 0.5, b = 0, l = 0.5), "cm"),
+#         legend.position = "top",
+#         legend.title = element_blank(),
+#         plot.title = element_text( hjust = -0.2, vjust = -10, size = 14)) +
+#   ggtitle("TP")
+# 
+# 
+# 
+# b <- ggplot(y, aes(x = group, y = SRP_Retention_percent, fill = group )) +
+#   geom_boxplot(position = position_dodge2(preserve = "single")) +
+#   scale_fill_manual(values = pal) +
+#   scale_x_discrete(labels = c("M", "R/C", "W")) +
+#   coord_cartesian(ylim = c(-180,120)) +
+#   facet_grid(.~flow, space = "free") +
+#   theme_bw(base_size = 10) +
+#   geom_text(data = summary, aes(label = count),
+#             position = position_dodge(width = 1.0), size = 2.9, color = "gray50")+
+#   geom_text(data = sig, aes(label = SRP), 
+#             position = position_dodge(width = 1.0), size = 2.5) +
+#   ylab("Retention (%)") +
+#   xlab(" ") +
+#   theme(plot.margin = unit(c(t = 0, r = 0.5, b = 0, l = 0.5), "cm"),
+#         legend.position = "none",
+#         legend.title = element_blank(),
+#         plot.title = element_text( hjust = -0.2, size = 14)) +
+#   ggtitle(expression("PO"[4]^"3-"))
+# 
+# 
+# plot_grid(a,b, nrow = 2, labels = c(" ", " "), rel_heights = c(1,0.9))
+# 
+# 
+# ##### Not this version
+# 
+# tiff(filename = "figures/Type_box_facet.tif", height=5, width=4, units= "in", res=800, compression= "lzw")
+# 
+# plot_grid(a,b, nrow = 2, labels = c(" ", " "), rel_heights = c(1,0.85))
+# 
+# dev.off()
 
 
 
@@ -157,7 +158,12 @@ y %>%
   group_by(group) %>%
   get_summary_stats(SRP_Retention_percent, type = "median_mad")
 
+
+
+
 y$newgroup <- paste(y$group, y$flow)
+
+
 y %>%
   group_by(newgroup) %>%
   get_summary_stats(TP_Retention_percent, type = "median_mad")
@@ -216,10 +222,11 @@ pwc
 pal <- c('#e5a77faa', '#3e783f33', '#6fabd9ee')
 
 a <- ggplot(y, aes(x = group, y = TP_Retention_percent, fill = group )) +
-  geom_boxplot(position = position_dodge2(preserve = "single")) +
+  geom_boxplot(position = position_dodge2(preserve = "single"), outlier.alpha = 0) +
   scale_fill_manual(values = pal) +
-  geom_point(position = position_jitterdodge(), alpha = 0.5, size = 2, aes(pch = flow2)) +
+  geom_point(position = position_jitterdodge(), alpha = 0.7, size = 2, aes(pch = flow2)) +
   #scale_color_manual(values = c("black", "gray70"))+
+  scale_shape_manual(values = c(17,1)) +
   scale_x_discrete(labels = c("M", "R/C", "W")) +
   coord_cartesian(ylim = c(-180,120)) +
   facet_grid(.~flow, space = "free") +
@@ -228,20 +235,20 @@ a <- ggplot(y, aes(x = group, y = TP_Retention_percent, fill = group )) +
             position = position_dodge(width = 1.0), size = 5, color = "gray50")+
   geom_text(data = sig, aes(label = TP), 
             position = position_dodge(width = 1.0), size = 5) +
-  ylab("Retention (%)") +
+  ylab("TP Retention (%)") +
   xlab(" ") +
-  theme(plot.margin = unit(c(t = 0, r = 0.5, b = 0, l = 0.5), "cm"),
-        legend.position = "top",
+  theme(plot.margin = unit(c(t = 0.3, r = 0.5, b = 0, l = 0.5), "cm"),
+        legend.position = "none",
         legend.title = element_blank(),
-        plot.title = element_text( hjust = -0.1, vjust = -10, size = 18)) +
-  ggtitle("TP")
-
+        plot.title = element_text( hjust = 0, vjust = 1, size = 13)) +
+  ggtitle("(A)                                                       (B)")
 
 
 b <- ggplot(y, aes(x = group, y = SRP_Retention_percent, fill = group )) +
-  geom_boxplot(position = position_dodge2(preserve = "single")) +
-  geom_point(position = position_jitterdodge(), size = 2, alpha = 0.5, aes(pch = flow2)) +
-    scale_fill_manual(values = pal) +
+  geom_boxplot(position = position_dodge2(preserve = "single"), outlier.alpha = 0) +
+  geom_point(position = position_jitterdodge(), size = 2, alpha = 0.7, aes(pch = flow2)) +
+  scale_shape_manual(values = c(17,1)) +
+  scale_fill_manual(values = pal) +
   scale_x_discrete(labels = c("M", "R/C", "W")) +
   coord_cartesian(ylim = c(-180,120)) +
   facet_grid(.~flow, space = "free") +
@@ -250,16 +257,16 @@ b <- ggplot(y, aes(x = group, y = SRP_Retention_percent, fill = group )) +
             position = position_dodge(width = 1.0), size = 5, color = "gray50")+
   geom_text(data = sig, aes(label = SRP), 
             position = position_dodge(width = 1.0), size = 5) +
-  ylab("Retention (%)") +
+  ylab(expression(paste("PO"[4]^"3-", " Retention (%)"))) +
   xlab(" ") +
-  theme(plot.margin = unit(c(t = 0, r = 0.5, b = 0, l = 0.5), "cm"),
-        legend.position = "none",
+  theme(plot.margin = unit(c(t = 0.0, r = 0.5, b = 0, l = 0.5), "cm"),
+        legend.position = "bottom",
         legend.title = element_blank(),
-        plot.title = element_text( hjust = -0.1, size = 18)) +
-  ggtitle(expression("PO"[4]^"3-"))
+        plot.title = element_text( hjust = 0.0, size = 13)) +
+  ggtitle("(C)                                                      (D)")
 
 
-#plot_grid(a,b, nrow = 2, labels = c(" ", " "), rel_heights = c(1,0.9))
+plot_grid(a,b, nrow = 2, labels = c(" ", " "), rel_heights = c(0.85, 1))
 
 
 
@@ -267,7 +274,7 @@ b <- ggplot(y, aes(x = group, y = SRP_Retention_percent, fill = group )) +
 
 tiff(filename = "figures/Type_box_facet_jitter.tif", height=7.5, width=7, units= "in", res=800, compression= "lzw")
 
-plot_grid(a,b, nrow = 2, labels = c(" ", " "), rel_heights = c(1,0.85))
+plot_grid(a,b, nrow = 2, labels = c(" ", " "), rel_heights = c(0.85,1))
 
 dev.off()
 
@@ -275,7 +282,7 @@ dev.off()
 
 
 
-
+  
 
 
 # ##### old versions
@@ -657,5 +664,48 @@ dev.off()
 
 
 
+ggplot(x, aes(x = group, y = TP_Inflow_mg_L, fill = group )) +
+  geom_boxplot(position = position_dodge2(preserve = "single"), outlier.alpha = 1) +
+  scale_y_continuous(trans='log10')
+  
+
+ggplot(x, aes(x = group, y = SRP_Inflow_mg_L, fill = group )) +
+  geom_boxplot(position = position_dodge2(preserve = "single"), outlier.alpha = 1) +
+  scale_y_continuous(trans='log10')
+
+ggplot(x, aes(x = group, y = TP_load_in_g_m2_yr, fill = group )) +
+  geom_boxplot(position = position_dodge2(preserve = "single"), outlier.alpha = 1) +
+  scale_y_continuous(trans='log10')
 
 
+ggplot(x, aes(x = group, y = SRP_load_in_g_m2_yr, fill = group )) +
+  geom_boxplot(position = position_dodge2(preserve = "single"), outlier.alpha = 1) +
+  scale_y_continuous(trans='log10')
+  
+
+x %>%
+  group_by(group) %>%
+  get_summary_stats(TP_Inflow_mg_L, type = "median_mad")
+
+pwc <- x %>%
+  pairwise_t_test(TP_Inflow_mg_L ~  group, p.adjust.method = 'bonferroni')
+pwc
+
+
+
+x %>%
+  group_by(group) %>%
+  get_summary_stats(TP_load_in_g_m2_yr, type = "median_mad")
+
+pwc <- x %>%
+  pairwise_t_test(TP_load_in_g_m2_yr ~  group, p.adjust.method = 'bonferroni')
+pwc
+
+
+x %>%
+  group_by(group) %>%
+  get_summary_stats(SRP_load_in_g_m2_yr, type = "median_mad")
+
+pwc <- x %>%
+  pairwise_t_test(SRP_load_in_g_m2_yr ~  group, p.adjust.method = 'bonferroni')
+pwc
